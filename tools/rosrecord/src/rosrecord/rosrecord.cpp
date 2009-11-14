254aaefb18bd34c96b8d144cbff3ca3508e10d9b
Jeremy Leibs <jleibs@gmail.com>
2009-11-14 01:56:06 +0000

    Adding initial blackbox recorder to rosrecord along with buffer limiting.

--- tools/rosrecord/src/rosrecord/rosrecord.cpp
+++ tools/rosrecord/src/rosrecord/rosrecord.cpp
@@ -40,9 +40,12 @@
 
 #include <boost/thread.hpp>
 
+#include <sstream>
 #include <string>
 #include <set>
 #include <queue>
+
+#include "std_msgs/Empty.h"
 
 class OutgoingMessage
 {
@@ -55,13 +58,26 @@ public:
   ros::Time time;
 };
 
+class OutgoingQueue
+{
+public:
+  OutgoingQueue(std::string _fname, std::queue<OutgoingMessage>* _queue, ros::Time _time) :
+    fname(_fname), queue(_queue), time(_time) {}
 
+  std::string fname;
+  std::queue<OutgoingMessage>* queue;
+  ros::Time time;
+};
 
-//! Global recorder instance so we can record from callback
-ros::record::Recorder g_recorder;
 
 //! Global verbose flag so we can easily use from callback
 bool g_verbose = false;
+
+//! Global blackbox flag so we can easily use from callback
+bool g_blackbox = false;
+
+// Global Eventual exit code
+int  g_exit_code       = 0;
 
 //! Global variable including the set of currenly recording topics
 std::set<std::string> g_currently_recording;
@@ -70,11 +86,33 @@ std::set<std::string> g_currently_recording;
 int g_count = -1;
 
 //! Global queue for storing 
-std::queue<OutgoingMessage> g_queue;
+std::queue<OutgoingMessage>* g_queue;
+
+//! Global queue size
+uint64_t g_queue_size=0;
+
+//! Global max queue size
+uint64_t g_max_queue_size=1048576*256;
+
+//! Queue of queues to be used by the blackbox recorders
+std::queue<OutgoingQueue> g_queue_queue;
 
 //! Mutex for global queue
 boost::mutex g_queue_mutex;
+
+//! Conditional variable for global queue
 boost::condition_variable_any g_queue_condition;
+
+
+template <class T>
+std::string time_to_str(T ros_t)
+{
+  char buf[1024]      = "";
+  time_t t = ros_t.sec;
+  struct tm *tms = localtime(&t);
+  strftime(buf, 1024, "%Y-%m-%d-%H-%M-%S", tms);
+  return std::string(buf);
+}
 
 //! Helper function to print executable usage
 void print_usage() {
@@ -93,28 +131,50 @@ void print_help() {
   fprintf(stderr, " -F <fname>  : Record to a file named exactly <fname>.bag\n");
   fprintf(stderr, " -a          : Record all published messages.\n");
   fprintf(stderr, " -v          : Display a message every time a message is received on a topic\n");
+  fprintf(stderr, " -m          : Maximize internal buffer size in MB (Default: 256MB)  0 = infinite.\n");
+  fprintf(stderr, " -b          : Enable black box recording (don't write to file unless triggered)\n");
   fprintf(stderr, " -h          : Display this help message\n");
 }
 
 
-//! Callback to be invoked to actually do the recording
+//! Callback to be invoked to save messages into a queue
 void do_queue(topic_tools::ShapeShifter::ConstPtr msg,
                std::string topic_name,
                boost::shared_ptr<ros::Subscriber> subscriber,
                boost::shared_ptr<int> count)
 {
+  ros::Time rectime = ros::Time::now();
+
   if (g_verbose)
     std::cout << "Received message on topic " << subscriber->getTopic() << std::endl;
 
-  // Actually record the message to file
-  //  g_recorder.record(topic_name, msg, ros::Time::now());
-  OutgoingMessage out(topic_name, msg, ros::Time::now());
+  OutgoingMessage out(topic_name, msg, rectime);
 
   {
     boost::mutex::scoped_lock lock(g_queue_mutex);
-    g_queue.push(out);
+    g_queue->push(out);
+    g_queue_size += out.msg->msgBufUsed;
+
+    while (g_max_queue_size > 0 && g_queue_size > g_max_queue_size)
+    {
+      OutgoingMessage drop = g_queue->front();
+      g_queue->pop();
+      g_queue_size -= drop.msg->msgBufUsed;
+      if (!g_blackbox)
+      {
+        static ros::Time last = ros::Time();
+        ros::Time now = ros::Time::now();
+        if (now > last + ros::Duration(5.0))
+        {
+          ROS_WARN("Rosrecord buffer exceeded.  Dropping oldest queued message.");
+          last = now;
+        }
+      }
+    }
   }
-  g_queue_condition.notify_all();
+  
+  if (!g_blackbox)
+    g_queue_condition.notify_all();
 
   // If we are book-keeping count, decrement and possibly shutdown
   if ((*count) > 0)
@@ -125,32 +185,143 @@ void do_queue(topic_tools::ShapeShifter::ConstPtr msg,
   }
 }
 
+//! Callback to be invoked to actually do the recording
+void blackbox_trigger(std_msgs::Empty::ConstPtr trigger, std::string prefix, bool add_date)
+{
+  ros::WallTime rectime = ros::WallTime::now();
+
+  std::vector<std::string> join;
+
+  if (prefix.length() > 0)
+    join.push_back(prefix);
+  if (add_date)
+    join.push_back(time_to_str(rectime));
+
+  std::string tgt_fname = join[0];
+  for (size_t i = 1; i < join.size(); i++)
+    tgt_fname = tgt_fname + "_" + join[i];
+
+  tgt_fname = tgt_fname + std::string(".bag");
+
+
+  ROS_INFO("Triggered blackbox recording with name %s.", tgt_fname.c_str());
+
+  {
+    boost::mutex::scoped_lock lock(g_queue_mutex);
+
+    OutgoingQueue out(tgt_fname, g_queue, ros::Time::now());
+
+    g_queue = new std::queue<OutgoingMessage>;
+    g_queue_size=0;
+
+    g_queue_queue.push(out);
+  }
+  g_queue_condition.notify_all();
+}
 
 //! Thread that actually does writing to file.
-// TODO: Do some checking here to make sure our queue isn't getting too big.
-void do_record()
+void do_record(std::string prefix, bool add_date)
 {
+  ros::WallTime rectime = ros::WallTime::now();
+
+  std::vector<std::string> join;
+
+  if (prefix.length() > 0)
+    join.push_back(prefix);
+  if (add_date)
+    join.push_back(time_to_str(rectime));
+
+  std::string tgt_fname = join[0];
+  for (size_t i = 1; i < join.size(); i++)
+    tgt_fname = tgt_fname + "_" + join[i];
+
+  tgt_fname = tgt_fname + std::string(".bag");
+
+  std::string fname = tgt_fname + std::string(".active");
+
   ros::NodeHandle nh;
+  ros::record::Recorder recorder;
+
+  // Open our recorder and add available topics
+  if (!recorder.open(std::string(fname)))
+  {
+    ROS_FATAL("Could not open output file: %s", fname.c_str());
+    g_exit_code = 1;
+    ros::shutdown();
+  }
   
   // Technically the g_queue_mutex should be locked while checking empty
   // Except it should only get checked if the node is not ok, and thus
   // it shouldn't be in contention.
-  while (nh.ok() || !g_queue.empty())
+  while (nh.ok() || !g_queue->empty())
   {
     boost::unique_lock<boost::mutex> lock(g_queue_mutex);
-    while(g_queue.empty())
+    while(g_queue->empty())
     {
       if (!nh.ok())
         return;
       g_queue_condition.wait(lock);
     }
 
-    OutgoingMessage out = g_queue.front();
-    g_queue.pop();
+    OutgoingMessage out = g_queue->front();
+    g_queue->pop();
+    g_queue_size -= out.msg->msgBufUsed;
 
     lock.release()->unlock();
 
-    g_recorder.record(out.topic_name, out.msg, out.time);
+    recorder.record(out.topic_name, out.msg, out.time);
+  }
+
+  // Close the file nicely
+  recorder.close();
+
+  // Rename the file to the actual target name
+  rename(fname.c_str(),tgt_fname.c_str());
+}
+
+
+void do_record_bb()
+{
+  ros::NodeHandle nh;
+  
+  while (nh.ok() || !g_queue_queue.empty())
+  {
+    boost::unique_lock<boost::mutex> lock(g_queue_mutex);
+    while(g_queue_queue.empty())
+    {
+      if (!nh.ok())
+        return;
+      g_queue_condition.wait(lock);
+    }
+
+    OutgoingQueue out_queue = g_queue_queue.front();
+    g_queue_queue.pop();
+
+    lock.release()->unlock();
+
+    std::string tgt_fname = out_queue.fname;
+    std::string fname = tgt_fname + std::string(".active");
+
+    ros::record::Recorder recorder;
+
+    if (recorder.open(fname))
+    {
+
+      while(!out_queue.queue->empty())
+      {
+        OutgoingMessage out = out_queue.queue->front();
+        out_queue.queue->pop();
+        recorder.record(out.topic_name, out.msg, out.time);
+      }
+
+      // Close the file nicely
+      recorder.close();
+
+      // Rename the file to the actual target name
+      rename(fname.c_str(),tgt_fname.c_str());
+    } else {
+      ROS_ERROR("Could not open file: %s", out_queue.fname.c_str());
+    }
   }
 }
 
@@ -198,51 +369,40 @@ int main(int argc, char **argv)
 
   // Variables
   bool check_master    = false; // Whether master should be checked periodically
-  int  exit_code       = 0;     // Eventual exit code
-  char name[1024]      = "";    // Name
-  char prefix[1024]    = "";    // Prefix
-  char tgt_fname[2048] = "";    // Eventual Target Filename
-  char fname[2048]     = "";    // Actual Filename (has .active appended until end)
 
-  // TODO: Change this to a ros-centric walltime call?
-  time_t t = ::time(NULL);
-  struct tm *tms = localtime(&t);
-  snprintf(name, sizeof(name), "%d-%02d-%02d-%02d-%02d-%02d-topic.bag",
-           tms->tm_year+1900, tms->tm_mon+1, tms->tm_mday,
-           tms->tm_hour     , tms->tm_min  , tms->tm_sec);
-
-
+  bool add_date = true;;
+  std::string prefix("");   // Prefix                                          
+  
   // Parse options  
   int option_char;
 
-  while ((option_char = getopt(argc,argv,"f:F:c:ahv")) != -1)
+  while ((option_char = getopt(argc,argv,"f:F:c:m:abhv")) != -1)
   {
     switch (option_char)
     {
-    case 'f': strncpy(prefix,optarg,sizeof(prefix)); break;
-    case 'F': strncpy(tgt_fname,optarg,sizeof(tgt_fname)); break;
+    case 'f': prefix = std::string(optarg); break;
+    case 'F': prefix = std::string(optarg); add_date = false; break;
     case 'c': g_count = atoi(optarg); break;
     case 'a': check_master = true; break;
+    case 'b': g_blackbox = true; break;
     case 'v': g_verbose = true; break;
+    case 'm': 
+      {
+        int m=0;
+        m=atoi(optarg);
+        if (m < 0)
+        {
+          fprintf(stderr, "Buffer size must be 0 or positive.\n");
+          return 1;
+        }
+        g_max_queue_size =  1048576*m;
+      }
+      break;
     case 'h': print_help(); return 1;
     case '?': print_usage(); return 1;
     }
   }
-  
-  // Create target name and actual (.active) name
-  if(strlen(tgt_fname) == 0)
-  {
-    if (strlen(prefix) == 0)
-      strcpy(tgt_fname, name);
-    else
-      snprintf(tgt_fname, sizeof(tgt_fname), "%s_%s", prefix, name);
-  }
-  else
-    strncpy(tgt_fname+strlen(tgt_fname),".bag",
-            sizeof(tgt_fname)-strlen(tgt_fname)-1);
-  
-  snprintf(fname, sizeof(fname), "%s.active", tgt_fname);
-  
+
   // Logic to make sure count is not specified with automatic topic
   // subscription (implied by no listed topics)
   if ((argc - optind) < 1)
@@ -259,15 +419,24 @@ int main(int argc, char **argv)
     }
   }
 
+  g_queue = new std::queue<OutgoingMessage>;
+
   // Get a node_handle
   ros::NodeHandle node_handle;
 
   // Only set up recording if we actually got a useful nodehandle.
   if (node_handle.ok())
   {
-  // Open our recorder and add available topics
-  if (g_recorder.open(std::string(fname)))
-  {
+    boost::thread record_thread;
+
+    // Spin up a thread for actually writing to file
+    if (!g_blackbox)
+      record_thread = boost::thread(boost::bind(&do_record, prefix, add_date));
+    else
+      record_thread = boost::thread(boost::bind(&do_record_bb));
+
+    ros::Subscriber trigger = node_handle.subscribe<std_msgs::Empty>("blackbox_trigger", 100, boost::bind(&blackbox_trigger, _1, prefix, add_date));
+
     // Every non-processed argument is assumed to be a topic
     for (int i = optind; i < argc; i++)
     {
@@ -275,36 +444,26 @@ int main(int argc, char **argv)
       boost::shared_ptr<ros::Subscriber> sub(new ros::Subscriber);
       *sub = node_handle.subscribe<topic_tools::ShapeShifter>(argv[i], 100, boost::bind(&do_queue, _1, argv[i], sub, count));
     }
-  } else {
-    exit_code = 1;
-    ros::shutdown();
-  }
 
-  ros::Timer check_master_timer;
-  if (check_master)
-    check_master_timer = node_handle.createTimer(ros::Duration(1.0), boost::bind(&do_check_master, _1, boost::ref(node_handle)));
-
-  ros::Timer check_subscribers_left_timer;
-  if (g_count >= 0)
-    check_subscribers_left_timer = node_handle.createTimer(ros::Duration(0.1), &do_check_subscribers_left);
-
-  // Spin up a thread for actually writing to file
-  boost::thread record_thread(&do_record);
-
-  ros::MultiThreadedSpinner s(10);
-  ros::spin(s);
-
-  g_queue_condition.notify_all();
-
-  record_thread.join();
-
-  // Close the file nicely
-  g_recorder.close();
-
-  // Rename the file to the actual target name
-  rename(fname,tgt_fname);
+    ros::Timer check_master_timer;
+    if (check_master)
+      check_master_timer = node_handle.createTimer(ros::Duration(1.0), boost::bind(&do_check_master, _1, boost::ref(node_handle)));
+    
+    ros::Timer check_subscribers_left_timer;
+    if (g_count >= 0)
+      check_subscribers_left_timer = node_handle.createTimer(ros::Duration(0.1), &do_check_subscribers_left);
+    
+    ros::MultiThreadedSpinner s(10);
+    ros::spin(s);
+    
+    g_queue_condition.notify_all();
+    
+    record_thread.join();
+        
   }
   
+  delete g_queue;
+
   // Return our exit code
-  return exit_code;
+  return g_exit_code;
 }

0d5367e6e79737416c5206fb66d8e8dd0a1cb16c
Josh Faust <jfaustwg@willowgarage.com>
2009-12-03 01:58:15 +0000

    Add debug::getBacktrace() function

---
+++ core/roslib/src/debug.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2009, Willow Garage, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "ros/debug.h"
+
+#if defined(__GNUC__)
+#include <execinfo.h>
+#endif
+
+#include <cstdlib>
+#include <sstream>
+
+namespace ros
+{
+namespace debug
+{
+
+std::string getBacktrace()
+{
+#if defined(__GNUC__)
+	void *array[20];
+	size_t size;
+	char **strings;
+	size_t i;
+
+	size = backtrace(array, 20);
+	strings = backtrace_symbols(array, size);
+
+	std::stringstream ss;
+
+	for (i = 1; i < size; i++)
+	{
+		ss << strings[i] << std::endl;
+	}
+
+	free (strings);
+
+	return ss.str();
+#else
+	return std::string();
+#endif
+}
+
+}
+
+}

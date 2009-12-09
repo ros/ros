ee9b87fc24e59466be0f41b6990b48d9edd0f55f
Rob Wheeler <wheeler@willowgarage.com>
2009-12-09 19:29:02 +0000

    select wxpython version 2.8

--- tools/rxtools/src/rxtools/rxplot_main.py
+++ tools/rxtools/src/rxtools/rxplot_main.py
@@ -32,6 +32,13 @@
 #
 # Revision $Id$
 
+WXVER = '2.8' 
+import wxversion 
+if wxversion.checkInstalled(WXVER): 
+  wxversion.select(WXVER) 
+else: 
+  print >> sys.stderr, "This application requires wxPython version %s"%(WXVER) 
+  sys.exit(1) 
 import wx
 
 import roslib.scriptutil

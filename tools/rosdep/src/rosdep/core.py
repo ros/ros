b733770156b0176d6c921e74dac7cbcfbab4111f
Tully Foote <tfoote@willowgarage.com>
2009-12-18 02:34:20 +0000

    reverting ubuntu version -> codename changes #2179

--- tools/rosdep/src/rosdep/core.py
+++ tools/rosdep/src/rosdep/core.py
@@ -158,17 +158,10 @@ class RosdepLookupPackage:
             else:
                 ## Hack to match rounding errors in pyyaml load 9.04  != 9.03999999999999996 in string space
                 for key in os_specific.keys():
-                    # NOTE: this hack fails if os_version is not major.minor
-                    if self.os_name == "ubuntu" and float(key) == 9.1 and self.os_version == "karmic":
-                        print >> sys.stderr, "Warning matched key '9.10' for %s please convert to using 'karmic'"%os_specific[key]
+                    if self.os_name == "ubuntu" and float(key) == float(self.os_version):
+                        #print "Matched %s"%(os_version)
+                        # NOTE: this hack fails if os_version is not major.minor
                         return os_specific[key]
-                    if self.os_name == "ubuntu" and float(key) == 9.04 and self.os_version == "jaunty":
-                        print >> sys.stderr, "Warning matched key '9.04' for %s please convert rosdep.yaml to using 'jaunty'"%os_specific[key]
-                        return os_specific[key]
-                    if self.os_name == "ubuntu" and float(key) == 8.04 and self.os_version == "hardy":
-                        print >> sys.stderr, "Warning matched key '8.04' for %s please convert to using 'hardy'"%os_specific[key]
-                        return os_specific[key]
-
                 print >> sys.stderr, "failed to find specific version %s of %s within"%(self.os_version, self.os_name), os_specific
                 return False                    
 
@@ -203,7 +196,7 @@ class RosdepLookupPackage:
 class Rosdep:
     def __init__(self, packages, command = "rosdep", robust = False):
         os_list = [debian.Ubuntu(), debian.Debian(), debian.Mint(), redhat.Fedora(), redhat.Rhel(), arch.Arch(), macports.Macports()]
-        self.osi = roslib.os_detect.OSAbstraction(os_list)
+        self.osi = roslib.os_detect.OSDetect(os_list)
         self.rosdeps = self.gather_rosdeps(packages, command)
         self.robust = robust
         

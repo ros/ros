cb4e8472924c616761134ff1df9b1ab95d011f8f
Ken Conley <kwc@kwc.org>
2010-02-02 18:04:54 +0000

    test_msgs: #2396 changing test_msgs layout to no longer confuse roswtf

--- test/test_msgs/run_tests.sh
+++ test/test_msgs/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf msg/cpp msg/test_cpp src
-`rospack find roslib`/scripts/genmsg msg/*.msg
-`rospack find genmsg_cpp`/genmsgtest msg/*.msg
-`rospack find roscpp`/scripts/genmsgtest msg/test_cpp
+`rospack find roslib`/scripts/genmsg msgtest/*.msg
+`rospack find genmsg_cpp`/genmsgtest msgtest/*.msg
+`rospack find roscpp`/scripts/genmsgtest msgtest/test_cpp

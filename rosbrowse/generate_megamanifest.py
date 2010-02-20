910e32d592b117da14966ad90b13361cc295c761
Ken Conley <kwc@kwc.org>
2010-02-20 10:19:11 +0000

    generate_megamanifest.py: #2456 re-calculate URLs for externals
    git-svn-id: file:///home/dthomas/wg/migration/svn2git/ros-svn/stacks/ros_experimental/trunk@8427 61973afe-1cd6-434e-a0a9-934cb0052259

--- rosbrowse/generate_megamanifest.py
+++ rosbrowse/generate_megamanifest.py
@@ -6,6 +6,7 @@ from subprocess import call, check_call, Popen
 
 from xml.dom.minidom import getDOMImplementation, parse
 import roslib.packages
+import roslib.vcs
 from roslib.manifest import MANIFEST_FILE
 
 with open('repos','r') as rf:
@@ -44,13 +45,29 @@ for name, uri in reps:
     repos_pkgs = {}
     for pkg in roslib.packages.list_pkgs_by_path(rel_path, cache=repos_pkgs):
         if pkg not in all_pkgs:
-            all_pkgs[pkg] = [pkg, name, uri, repos_pkgs[pkg][0]]
+            # de-resolve externals #2456. This logic assumes that the
+            # vcs is SVN and that they URLs are consistently
+            # specified. It will probably take a lot more logic to get
+            # this perfectly right
+            override_path = None
+            try:
+                pkg_path = repos_pkgs[pkg][0]
+                vcs, true_uri = roslib.vcs.guess_vcs_uri(pkg_path)
+                # rel_path is incorrect if true_uri doesn't
+                # match. recompute rel_path by getting URL of package
+                # and slicing.
+                if vcs == 'svn' and true_uri != uri:
+                    url_path = roslib.vcs.get_svn_url(pkg_path)
+                    override_path = url_path[len(true_uri):]
+            except:
+                true_uri = uri
+            all_pkgs[pkg] = [pkg, name, true_uri, pkg_path, override_path]
 
 pkgs_dom = getDOMImplementation().createDocument(None, 'pkgs', None)
 pkgs_node = pkgs_dom.documentElement
 
 for k, v in all_pkgs.iteritems():
-    pkg, repo, uri, rel_path = v
+    pkg, repo, uri, rel_path, override_path = v
     manifest_p = rel_path+os.sep+MANIFEST_FILE
     try:
         manifest = parse(manifest_p)
@@ -59,7 +76,10 @@ for k, v in all_pkgs.iteritems():
         manifest_node.setAttribute("repo", repo)
         manifest_node.setAttribute("repo_host", uri)
         rel_path = rel_path[len('checkouts/'+repo):]
-        manifest_node.setAttribute("path", rel_path)
+        if override_path:
+            manifest_node.setAttribute("path", override_path)
+        else:
+            manifest_node.setAttribute("path", rel_path)            
         pkgs_node.appendChild(manifest_node)
     except:
         import traceback

8c4578e35cdf299cdbdc01c0486aaa5532143828
Josh Faust <jfaustwg@willowgarage.com>
2010-02-27 03:31:26 +0000

    aligned allocator
    git-svn-id: file:///home/dthomas/wg/migration/svn2git/ros-svn/stacks/ros_experimental/trunk@8510 61973afe-1cd6-434e-a0a9-934cb0052259

---
+++ rosrt/include/ros/rt/aligned_alloc.h
@@ -0,0 +1,165 @@
+
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2010, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+#ifndef ROSRT_ALIGNED_ALLOC_H
+#define ROSRT_ALIGNED_ALLOC_H
+
+#include <new>
+#include <ros/assert.h>
+#include <ros/atomic.h>
+
+namespace ros
+{
+namespace rt
+{
+
+// alignedMalloc/alignedFree implementations found at
+// http://www.gamasutra.com/view/feature/3942/data_alignment_part_1.php?page=2
+
+void* alignedMalloc(size_t size, size_t alignment)
+{
+  const int pointerSize = sizeof(void*);
+  const int requestedSize = size + alignment - 1 + pointerSize;
+  void* raw = std::malloc(requestedSize);
+
+  if (!raw)
+  {
+    return 0;
+  }
+
+  void* start = (uint8_t*)raw + pointerSize;
+  void* aligned = (void*)(((uintptr_t)((uint8_t*)start+alignment-1)) & ~(alignment-1));
+  *(void**)((uint8_t*)aligned-pointerSize) = raw;
+  return aligned;
+}
+
+void alignedFree(void* aligned)
+{
+  if (!aligned)
+  {
+    return;
+  }
+
+  void* raw = *(void**)((uint8_t*)aligned-sizeof(void*));
+  std::free(raw);
+}
+
+template<class T, size_t align> class AlignedAllocator;
+
+// specialize for void:
+template<size_t align>
+class AlignedAllocator<void, align>
+{
+public:
+  typedef void* pointer;
+  typedef const void* const_pointer;
+  // reference to void members are impossible.
+  typedef void value_type;
+
+  template<class U>
+  struct rebind
+  {
+    typedef AlignedAllocator<U, align> other;
+  };
+};
+
+template<class T, size_t align>
+class AlignedAllocator
+{
+public:
+  typedef size_t size_type;
+  typedef ptrdiff_t difference_type;
+  typedef T* pointer;
+  typedef const T* const_pointer;
+  typedef T& reference;
+  typedef const T& const_reference;
+  typedef T value_type;
+
+  template<class U>
+  struct rebind
+  {
+    typedef AlignedAllocator<U, align> other;
+  };
+
+  AlignedAllocator() throw ()
+  {
+  }
+
+  ~AlignedAllocator() throw ()
+  {
+  }
+
+  pointer address(reference r) const
+  {
+    return &r;
+  }
+  const_pointer address(const_reference r) const
+  {
+    return &r;
+  }
+  size_type max_size() const throw ()
+  {
+    return ~size_type(0);
+  }
+  pointer allocate(size_type n, typename AlignedAllocator<void, align>::const_pointer hint = 0)
+  {
+    void* mem = alignedMalloc(n * sizeof(T), align);
+    if (!mem)
+    {
+      throw std::bad_alloc();
+    }
+
+    return static_cast<pointer>(mem);
+  }
+  void deallocate(pointer p, size_type n)
+  {
+    alignedFree(p);
+  }
+
+  void construct(pointer p, const_reference val)
+  {
+    new (p) T(val);
+  }
+  void destroy(pointer p)
+  {
+    p->~T();
+  }
+};
+
+}
+}
+
+#endif // ROSRT_ALIGNED_ALLOC_H
+
---
+++ rosrt/test/test_aligned_alloc.cpp
@@ -0,0 +1,67 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2010, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "ros/rt/aligned_alloc.h"
+
+using namespace ros::rt;
+
+TEST(AlignedAlloc, mallocfree)
+{
+  for (size_t i = 1;i < 100000; ++i)
+  {
+    void* mem = alignedMalloc(i, 128);
+    ASSERT_TRUE((((uintptr_t)mem) & 127) == 0);
+
+    alignedFree(mem);
+  }
+}
+
+TEST(AlignedAlloc, stlAllocator)
+{
+  for (size_t i = 1;i < 20000; ++i)
+  {
+    std::vector<uint8_t, AlignedAllocator<void, 128> > v;
+    v.resize(i);
+    ASSERT_TRUE((((uintptr_t)&v.front()) & 127) == 0);
+  }
+}
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+

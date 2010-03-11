b5070ecde1a05980fee6f4377d5f30d4ec188051
Tully Foote <tfoote@willowgarage.com>
2010-03-11 20:36:39 +0000

    don't clean rospack if NOBUILD present #2443

--- tools/rosmake/src/rosmake/rosmake.py
+++ tools/rosmake/src/rosmake/rosmake.py
@@ -686,7 +686,7 @@ class RosMakeAll:
         if options.pre_clean:
           build_queue = parallel_build.BuildQueue(self.build_list, self.dependency_tracker, robust_build = True)
           self.parallel_build_pkgs(build_queue, "clean", threads = options.threads)
-          if "rospack" in self.build_list:
+          if "rospack" in self.build_list and not self.flag_tracker.has_nobuild("rospack"):
               self.print_all( "Rosmake detected that rospack was requested to be cleaned.  Cleaning it for it was skipped earlier.")
               subprocess.check_call(["make", "-C", os.path.join(os.environ["ROS_ROOT"], "tools/rospack"), "clean"])
 
@@ -701,7 +701,7 @@ class RosMakeAll:
           build_queue.register_prebuilt(["rospack", "gtest", "genmsg_cpp"])
 
           build_passed = self.parallel_build_pkgs(build_queue, options.target, threads = options.threads)
-          if "rospack" in self.build_list and options.target == "clean":
+          if "rospack" in self.build_list and options.target == "clean" and not self.flag_tracker.has_nobuild("rospack"):
               self.print_all( "Rosmake detected that rospack was requested to be cleaned.  Cleaning it, because it was skipped earlier.")
               subprocess.check_call(["make", "-C", os.path.join(os.environ["ROS_ROOT"], "tools/rospack"), "clean"])
 

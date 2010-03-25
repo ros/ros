939bc836a9fd848646997229bda0d0167ddefde7
Lorenz Moesenlechner <moesenle@in.tum.de>
2010-03-25 20:02:42 +0000

    Lisp messages use the ASDF::ROSLISP-MSG-SOURCE-FILE component class now.
    That means they are compiled into $ROS_HOME/roslisp/...

--- core/genmsg_cpp/genmsg_main_lisp.cpp
+++ core/genmsg_cpp/genmsg_main_lisp.cpp
@@ -173,6 +173,7 @@ public:
     }
     fprintf(f, "\n(in-package :asdf)\n\n");
     fprintf(f, "(defsystem \"%s-msg\"\n"
+        "  :default-component-class roslisp-msg-source-file\n"      
         "  :depends-on (:roslisp-msg-protocol :roslisp-utils", g_pkg.c_str());
 
     for(set<string>::iterator it = dependencies.begin();

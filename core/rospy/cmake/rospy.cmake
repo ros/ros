3380c098201b17dde45d7f2d067858790f34f1d6
Ken Conley <kwc@kwc.org>
2010-04-09 00:41:13 +0000

    rospy: updated to use gensrv_py.py directly and removed symlinks

--- core/rospy/cmake/rospy.cmake
+++ core/rospy/cmake/rospy.cmake
@@ -51,7 +51,7 @@ macro(gensrv_py)
   rosbuild_get_srvs(_srvlist)
   set(_inlist "")
   set(_autogen "")
-  set(gensrv_py_exe ${rospy_PACKAGE_PATH}/scripts/gensrv_py)
+  set(gensrv_py_exe ${rospy_PACKAGE_PATH}/scripts/gensrv_py.py)
 
   foreach(_srv ${_srvlist})
     # Construct the path to the .srv file
--- core/rospy/scripts/genmsg_py.py
+++ core/rospy/scripts/genmsg_py.py
@@ -38,6 +38,7 @@ ROS message source code generation for Python
 
 Converts ROS .msg files in a package into Python source code implementations.
 """
+
 import roslib; roslib.load_manifest('rospy')
 
 import sys

45a2ee6b08685371cbf601a22117a8047a554041
Bhaskara Marthi <bhaskara@willowgarage.com>
2010-05-08 04:40:52 +0000

    make the default system suggestion smarter in ros-load-system; make it change to the package root

--- tools/rosemacs/slime-ros.el
+++ tools/rosemacs/slime-ros.el
@@ -18,15 +18,19 @@
          (let ((default (slime-eval `(cl:identity ros-load:*current-ros-package*))))
            (ros-completing-read-package nil default slime-ros-completion-function)))))
 
+(defun slime-ros-replace-underscores (str)
+  (replace-regexp-in-string "_" "-" str))
+
 (defun slime-ros-get-systems-in-pkg (package &optional default-value prompt)
   (let* ((package-path (ros-package-path package))
          (asd-files (ros-files-in-package package-path "asd" "asdf"))
-         (default (when (member default-value asd-files)
-                    default-value))
+         (default2 (slime-ros-replace-underscores default-value))
+         (default (cond ((member default-value asd-files) default-value)
+                        ((member default2 asd-files) default2)))
          (prompt (concat (or prompt (format "ROS Package `%s', System" package))
                          (if default
                              (format " (default `%s'): " default)
-                             ": "))))
+                           ": "))))
     (funcall slime-ros-completion-function
              prompt (slime-bogus-completion-alist asd-files)
              nil nil nil nil default)))
@@ -36,6 +40,7 @@
               (interactive)
               (let* ((ros-pkg-name (slime-ros-read-pkg-name))
                      (system-name (slime-ros-get-systems-in-pkg ros-pkg-name ros-pkg-name)))
+                (slime-cd (ros-package-path ros-pkg-name))
                 (slime-eval `(cl:setf ros-load:*current-ros-package* ,ros-pkg-name))
                 (slime-oos system-name 'load-op)))))
 

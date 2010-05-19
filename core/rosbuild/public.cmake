f90bd0365e5aa5f2b035f5685b11bacc3422b236
Brian Gerkey <brian@gerkey.org>
2010-05-19 23:53:13 +0000

    Added rosbuild_check_for_sse

--- core/rosbuild/public.cmake
+++ core/rosbuild/public.cmake
@@ -1041,3 +1041,87 @@ macro(rosbuild_add_swigpy_library target lib)
   endif(APPLE)
 endmacro(rosbuild_add_swigpy_library)
 
+macro(rosbuild_check_for_sse)
+  # check for SSE extensions
+  include(CheckCXXSourceRuns)
+  if( CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX )
+   set(SSE_FLAGS)
+  
+   set(CMAKE_REQUIRED_FLAGS "-msse3")
+   check_cxx_source_runs("
+    #include <pmmintrin.h>
+  
+    int main()
+    {
+       __m128d a, b;
+       double vals[2] = {0};
+       a = _mm_loadu_pd(vals);
+       b = _mm_hadd_pd(a,a);
+       _mm_storeu_pd(vals, b);
+       return 0;
+    }"
+    HAS_SSE3_EXTENSIONS)
+  
+   set(CMAKE_REQUIRED_FLAGS "-msse2")
+   check_cxx_source_runs("
+    #include <emmintrin.h>
+  
+    int main()
+    {
+        __m128d a, b;
+        double vals[2] = {0};
+        a = _mm_loadu_pd(vals);
+        b = _mm_add_pd(a,a);
+        _mm_storeu_pd(vals,b);
+        return 0;
+     }"
+     HAS_SSE2_EXTENSIONS)
+  
+   set(CMAKE_REQUIRED_FLAGS "-msse")
+   check_cxx_source_runs("
+    #include <xmmintrin.h>
+    int main()
+    {
+        __m128 a, b;
+        float vals[4] = {0};
+        a = _mm_loadu_ps(vals);
+        b = a;
+        b = _mm_add_ps(a,b);
+        _mm_storeu_ps(vals,b);
+        return 0;
+    }"
+    HAS_SSE_EXTENSIONS)
+  
+   set(CMAKE_REQUIRED_FLAGS)
+  
+   if(HAS_SSE3_EXTENSIONS)
+    set(SSE_FLAGS "-msse3 -mfpmath=sse")
+    message(STATUS "[rosbuild] Found SSE3 extensions, using flags: ${SSE_FLAGS}")
+   elseif(HAS_SSE2_EXTENSIONS)
+    set(SSE_FLAGS "-msse2 -mfpmath=sse")
+    message(STATUS "[rosbuild] Found SSE2 extensions, using flags: ${SSE_FLAGS}")
+   elseif(HAS_SSE_EXTENSIONS)
+    set(SSE_FLAGS "-msse -mfpmath=sse")
+    message(STATUS "[rosbuild] Found SSE extensions, using flags: ${SSE_FLAGS}")
+   endif()
+  elseif(MSVC)
+   check_cxx_source_runs("
+    #include <emmintrin.h>
+  
+    int main()
+    {
+        __m128d a, b;
+        double vals[2] = {0};
+        a = _mm_loadu_pd(vals);
+        b = _mm_add_pd(a,a);
+        _mm_storeu_pd(vals,b);
+        return 0;
+     }"
+     HAS_SSE2_EXTENSIONS)
+   if( HAS_SSE2_EXTENSIONS )
+    message(STATUS "[rosbuild] Found SSE2 extensions")
+    set(SSE_FLAGS "/arch:SSE2 /fp:fast -D__SSE__ -D__SSE2__" )
+   endif()
+  endif()
+endmacro(rosbuild_check_for_sse)
+

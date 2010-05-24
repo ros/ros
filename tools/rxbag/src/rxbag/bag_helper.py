ffc246568e51809044d96f66f56f4c792a9b12f1
Tim Field <tfield@willowgarage.com>
2010-05-24 07:50:54 +0000

    rxbag: documenting bag_helper functions

--- tools/rxbag/src/rxbag/bag_helper.py
+++ tools/rxbag/src/rxbag/bag_helper.py
@@ -29,15 +29,25 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
+
+"""
+Helper functions for bag files and timestamps.
+"""
 
 PKG = 'rxbag'
 import roslib; roslib.load_manifest(PKG)
-import rospy
+
 import time
 
+import rospy
+
 def stamp_to_str(t):
+    """
+    Convert a rospy.Time to a human-readable string.
+    
+    @param t: time to convert
+    @type  t: rospy.Time
+    """
     t_sec = t.to_sec()
     if t < rospy.Time.from_sec(60 * 60 * 24 * 7 * 52 * 5):
         # Display timestamps earlier than 1975 as seconds
@@ -45,9 +55,24 @@ def stamp_to_str(t):
     else:
         return time.strftime('%b %d %Y %H:%M:%S', time.localtime(t_sec)) + '.%03d' % (t.nsecs / 1000000)
 
-def get_topics(bag): return sorted(set([c.topic for c in bag._get_connections()]))
+def get_topics(bag):
+    """
+    Get an alphabetical list of all the unique topics in the bag.
+    
+    @return: sorted list of topics
+    @rtype:  list of str
+    """
+    return sorted(set([c.topic for c in bag._get_connections()]))
 
 def get_start_stamp(bag):
+    """
+    Get the earliest timestamp in the bag.
+    
+    @param bag: bag file
+    @type  bag: rosbag.Bag
+    @return: earliest timestamp
+    @rtype:  rospy.Time
+    """
     start_stamp = None
     for connection_start_stamp in [index[0].time for index in bag._connection_indexes.values()]:
         if not start_stamp or connection_start_stamp < start_stamp:
@@ -55,20 +80,46 @@ def get_start_stamp(bag):
     return start_stamp
 
 def get_end_stamp(bag):
+    """
+    Get the latest timestamp in the bag.
+    
+    @param bag: bag file
+    @type  bag: rosbag.Bag
+    @return: latest timestamp
+    @rtype:  rospy.Time
+    """
     end_stamp = None
     for connection_end_stamp in [index[-1].time for index in bag._connection_indexes.values()]:
-        if not end_stamp or connection_end_stamp < end_stamp:
+        if not end_stamp or connection_end_stamp > end_stamp:
             end_stamp = connection_end_stamp
 
     return end_stamp
 
 def get_topics_by_datatype(bag):
+    """
+    Get all the message types in the bag and their associated topics.
+    
+    @param bag: bag file
+    @type  bag: rosbag.Bag
+    @return: mapping from message typename to list of topics
+    @rtype:  dict of str to list of str
+    """
     topics_by_datatype = {}
     for c in bag._get_connections():
         topics_by_datatype.setdefault(c.datatype, []).append(c.topic)
+    
     return topics_by_datatype
 
 def get_datatype(bag, topic):
+    """
+    Get the datatype of the given topic.
+    
+    @param bag: bag file
+    @type  bag: rosbag.Bag
+    @return: message typename
+    @rtype:  str
+    """
     for c in bag._get_connections(topic):
         return c.datatype
+
     return None

2f91134a2b37748a22faf945742b9dc4fe26f6d8
Tully Foote <tfoote@willowgarage.com>
2010-05-25 19:36:32 +0000

    returning result due to success or not

--- tools/rosmake/src/rosmake/parallel_build.py
+++ tools/rosmake/src/rosmake/parallel_build.py
@@ -139,10 +139,10 @@ class CompileThread(threading.Thread):
         self.rosmakeall.printer.print_all ("Starting >>> %s [ make ] "%pkg,  thread_name=self.name)
       (result, result_string) = self.rosmakeall.build(pkg, self.argument, self.build_queue.robust_build) 
       self.rosmakeall.printer.print_all("Finished <<< %s %s"%(pkg, result_string), thread_name= self.name)
+      self.build_queue.return_built(pkg, result)
+      print "returned"
       if result or self.build_queue.robust_build:
         print "result or robust"
-        self.build_queue.return_built(pkg, result)
-        print "returned"
         if result_string.find("[Interrupted]") != -1:
           self.rosmakeall.printer.print_all("Caught Interruption", thread_name=self.name)
           self.build_queue.stop() #todo move this logic into BuildQueue itself

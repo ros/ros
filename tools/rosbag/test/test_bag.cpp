107f65852c31e30541f7b0c5c12922067bdbad36
Josh Faust <jfaustwg@willowgarage.com>
2010-06-14 22:52:34 +0000

    fix rosbag test for new time changes

--- tools/rosbag/test/test_bag.cpp
+++ tools/rosbag/test/test_bag.cpp
@@ -527,6 +527,7 @@ TEST(rosbag, modify_bag_works) {
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "test_bag");
+    ros::NodeHandle nh;
 
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();

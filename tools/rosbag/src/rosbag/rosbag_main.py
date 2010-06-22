bea5e4794a7d6e293bee1e24c926efe0b202188f
Tim Field <tfield@willowgarage.com>
2010-06-22 20:11:35 +0000

    rosbag: fix spelling

--- tools/rosbag/src/rosbag/rosbag_main.py
+++ tools/rosbag/src/rosbag/rosbag_main.py
@@ -150,7 +150,7 @@ def play_cmd(argv):
     parser.add_option("-r", "--rate",         dest="rate",       default=1.0,   type='float', action="store", help="multiply the publish rate by FACTOR", metavar="FACTOR")
     parser.add_option("-s", "--start",        dest="start",      default=0.0,   type='float', action="store", help="start SEC seconds into the bag files", metavar="SEC")
     parser.add_option("-l", "--loop",         dest="loop",       default=False, action="store_true", help="loop playback")
-    parser.add_option("-k", "--keep-alive",   dest="keep_alive", default=False, action="store_true", help="keep alive past end of bag (useful for publishg latched topics)")
+    parser.add_option("-k", "--keep-alive",   dest="keep_alive", default=False, action="store_true", help="keep alive past end of bag (useful for publishing latched topics)")
     parser.add_option("--try-future-version", dest="try_future", default=False, action="store_true", help="still try to open a bag file, even if the version number is not known to the player")
 
     (options, args) = parser.parse_args(argv)

f3a78958c1b2c18d8e3bfa16102f98677fd613c3
Brian Gerkey <brian@gerkey.org>
2010-07-14 06:25:51 +0000

    Added preexec_fn to Popen call to make, in an attempt to fix esoteric gzip errors in eml

--- tools/rosmake/src/rosmake/rosmake.py
+++ tools/rosmake/src/rosmake/rosmake.py
@@ -45,6 +45,7 @@ import roslib.rosenv
 import roslib.stacks
 import threading
 import math
+import signal
 
 from operator import itemgetter
 
@@ -359,6 +360,13 @@ class RosMakeAll:
         build_passed = build_queue.succeeded() and all_pkgs_passed
         return build_passed
 
+    # This function taken from
+    # http://www.chiark.greenend.org.uk/ucgi/~cjwatson/blosxom/2009-07-02-python-sigpipe.html
+    def _subprocess_setup(self):
+        # Python installs a SIGPIPE handler by default. This is usually not
+        # what non-Python subprocesses expect.
+	signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+
     def _build_package(self, package, argument=None):
         """
         Lower-level routine for building a package. Handles execution of actual build command.
@@ -375,7 +383,7 @@ class RosMakeAll:
         if argument:
             cmd[-1] += argument
         self.printer.print_full_verbose (cmd)
-        command_line = subprocess.Popen(cmd, stdout=subprocess.PIPE,  stderr=subprocess.STDOUT, env=local_env)
+        command_line = subprocess.Popen(cmd, stdout=subprocess.PIPE,  stderr=subprocess.STDOUT, env=local_env, preexec_fn=self._subprocess_setup)
         (pstd_out, pstd_err) = command_line.communicate() # pstd_err should be None due to pipe above
         return (command_line.returncode, pstd_out)
 

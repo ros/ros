b314d451df51818afc29b0e92f41641fcf1175b4
Bhaskara Marthi <bhaskara@willowgarage.com>
2010-07-20 06:10:57 +0000

    don't query about polling processes on exit

--- tools/rosemacs/rosemacs.el
+++ tools/rosemacs/rosemacs.el
@@ -1,3 +1,38 @@
+;;; rosemacs.el --- Tools for ROS users
+
+;; Copyright (c) 2008, Willow Garage, Inc.
+;; All rights reserved.
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are met:
+;;
+;;     * Redistributions of source code must retain the above copyright
+;;       notice, this list of conditions and the following disclaimer.
+;;     * Redistributions in binary form must reproduce the above copyright
+;;       notice, this list of conditions and the following disclaimer in the
+;;       documentation and/or other materials provided with the distribution.
+;;     * Neither the name of the Willow Garage, Inc. nor the names of its
+;;       contributors may be used to endorse or promote products derived from
+;;       this software without specific prior written permission.
+;;
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;; POSSIBILITY OF SUCH DAMAGE.
+;;
+
+;; Author: Bhaskara Marthi
+;; Keywords: tools, convenience
+
+;;; Commentary:
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Installation instructions
 ;; 1. Put this file somewhere (if it's not already in 
@@ -32,7 +67,8 @@
 ;;    by \C-h to see the list of added commands.
 ;;
 ;; 1. Directory tracking and tab completion for rosbash 
-;;    commands should now work correctly in shell mode
+;;    commands, including rostopic and rosnode, should now
+;;    work correctly in shell mode
 ;;
 ;; 2. The commands {find|view}-ros-{file|message|service}, 
 ;;    and view-most-recent-ros-log for navigating the ros
@@ -56,6 +92,8 @@
 ;;    Within that buffer, k to kill, r to relaunch.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;;; Code:
 
 (require 'shell)
 (require 'cl)
@@ -599,6 +637,7 @@ parameter."
         (delete-process old-proc))
       (when (> interval 0)
         (let ((proc (start-process name name (concat rosemacs/pathname "poll-rosnode") (format "%s" interval))))
+          (set-process-query-on-exit-flag proc nil)
           (set-process-filter proc 'rosemacs/rosnode-filter)))
       )))
 
@@ -636,6 +675,7 @@ parameter."
         ))
     (when (> interval 0)
       (let ((proc (start-process name name (concat rosemacs/pathname "poll-rostopic")  (format "%s" interval))))
+        (set-process-query-on-exit-flag proc nil)
         (set-process-filter proc 'rosemacs-topic-filter)))))
 
 (defun display-ros-topic-info ()
@@ -1311,3 +1351,6 @@ k kills the process (sends SIGINT)"
 
     
 (provide 'rosemacs)
+
+
+;;; rosemacs.el ends here

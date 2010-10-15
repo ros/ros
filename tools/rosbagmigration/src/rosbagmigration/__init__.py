abb8a9a4c69180229fc46d75109782e0ed343791
Tim Field <tfield@willowgarage.com>
2010-10-15 01:02:29 +0000

    rosbagmigration: adding deprecation warning

--- tools/rosbagmigration/src/rosbagmigration/__init__.py
+++ tools/rosbagmigration/src/rosbagmigration/__init__.py
@@ -1,1 +1,4 @@
+import warnings
+warnings.warn('rosbagmigration is deprecated; use the rosbag package (http://www.ros.org/wiki/rosbag) instead', category=DeprecationWarning)
+
 from rosbag.migration import *

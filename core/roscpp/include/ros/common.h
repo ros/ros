6c980913b060cfff693f586a7d824c11164990db
Ken Conley <kwc@kwc.org>
2010-10-22 20:59:48 +0000

    preparing 1.3.0 release

--- core/roscpp/include/ros/common.h
+++ core/roscpp/include/ros/common.h
@@ -40,8 +40,8 @@
 #include <boost/shared_array.hpp>
 
 #define ROS_VERSION_MAJOR 1
-#define ROS_VERSION_MINOR 1
-#define ROS_VERSION_PATCH 16
+#define ROS_VERSION_MINOR 3
+#define ROS_VERSION_PATCH 0
 #define ROS_VERSION_COMBINED(major, minor, patch) (((major) << 20) | ((minor) << 10) | (patch))
 #define ROS_VERSION ROS_VERSION_COMBINED(ROS_VERSION_MAJOR, ROS_VERSION_MINOR, ROS_VERSION_PATCH)
 

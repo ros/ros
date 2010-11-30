3247188bed39fac54d8da794c24802f78bcdadd7
Tully Foote <tfoote@willowgarage.com>
2010-11-30 19:47:10 +0000

    fixing write to be inside of with open block, this was causing the I/O operation on closed file

--- tools/rosunit/scripts/clean_junit_xml.py
+++ tools/rosunit/scripts/clean_junit_xml.py
@@ -85,7 +85,7 @@ def clean_results(test_results_dir, output_dir, filter):
                 output_path = os.path.join(output_dir, "%s.xml"%test_name)
                 with open(output_path, 'w') as f:
                     print "re-writing", output_path
-                f.write(result.xml().encode('utf-8'))
+                    f.write(result.xml().encode('utf-8'))
             except Exception, e:
                 print >> sys.stderr, "ignoring [%s]: %s"%(file, e)
 

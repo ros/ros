d8d4e84eea95ce0d51fd45f69822a01310e045e4
straszheim <troy@industrial-perception.com>
2011-01-25 23:48:55 +0000

    bug #3271,
    https://code.ros.org/trac/ros/ticket/3271

--- clients/cpp/roscpp/src/libros/init.cpp
+++ clients/cpp/roscpp/src/libros/init.cpp
@@ -540,6 +540,14 @@ void shutdown()
   logger->removeAppender(g_rosout_appender);
   g_rosout_appender = 0;
 
+  // reset this so that the logger doesn't get crashily destroyed
+  // again during global destruction.  
+  //
+  // See https://code.ros.org/trac/ros/ticket/3271
+  //
+  log4cxx::LoggerPtr& fo_logger = ros::file_log::getFileOnlyLogger();
+  fo_logger = log4cxx::LoggerPtr();
+  
   if (g_started)
   {
     TopicManager::instance()->shutdown();

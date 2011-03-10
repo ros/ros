50f9581f5c1aaad8490ec0362148dfa198cb02d9
straszheim <troy@industrial-perception.com>
2011-03-10 23:23:40 +0000

    rosbuild2 stuff

---
+++ clients/cpp/roscpp/cmake/rosbuild2.cmake
@@ -0,0 +1,101 @@
+set(genmsg_cpp_exe ${roscpp_PACKAGE_PATH}/scripts/genmsg_cpp.py)
+# Message-generation support.
+macro(genmsg_cpp TYPE)
+
+  foreach(_msg ${ARGN})
+    # Construct the path to the .msg file
+    if (${TYPE} STREQUAL "STATIC")
+      set(_input ${PROJECT_SOURCE_DIR}/${_msg})
+      rosbuild_assert_file_exists(${_input})
+    elseif(${TYPE} STREQUAL "GENERATED")
+      set(_input ${_msg})
+    else()
+      message(FATAL_ERROR "Unknown message type \"${TYPE}\" (must be either STATIC or GENERATED)")
+    endif()
+
+    get_filename_component(_fname ${_msg} NAME)
+
+    string(REPLACE ".msg" ".h" _output_cpp_base ${_fname})
+
+    set(_outdir ${ROSBUILD_GEN_DIR}/cpp)
+    set(_output_cpp ${_outdir}/${PROJECT_NAME}/${_output_cpp_base})
+
+    list(APPEND ${PROJECT_NAME}_generated ${_output_cpp})
+
+    set(_incflags "")
+    foreach(dir ${DEPENDED_PACKAGE_PATHS})
+      list(APPEND _incflags -I${dir})
+    endforeach()
+
+    # Add the rule to build the .h the .msg
+    add_custom_command(
+      OUTPUT ${_output_cpp} 
+      COMMAND ${ROSBUILD_SUBSHELL} 
+      ${genmsg_cpp_exe} 
+      ${_input}
+      -p ${PROJECT_NAME}
+      -o ${_outdir}
+      -I${CMAKE_CURRENT_BINARY_DIR} ${_incflags} -I${CMAKE_CURRENT_SOURCE_DIR}
+      DEPENDS ${_input} ${genmsg_cpp_exe} ${gendeps_exe}
+      COMMENT "${PROJECT_NAME}: generating ${_output_cpp_base}"
+      )
+  endforeach(_msg)
+
+endmacro(genmsg_cpp)
+
+
+set(gensrv_cpp_exe ${CMAKE_SOURCE_DIR}/ros_comm/clients/cpp/roscpp/scripts/gensrv_cpp.py)
+# Service-generation support.
+macro(gensrv_cpp TYPE)
+
+  foreach(_srv ${ARGN})
+    # Construct the path to the .srv file
+
+    # Construct the path to the .msg file
+    if (${TYPE} STREQUAL "STATIC")
+      set(_input ${PROJECT_SOURCE_DIR}/${_srv})
+    elseif(${TYPE} STREQUAL "GENERATED")
+      set(_input ${CMAKE_CURRENT_BINARY_DIR}/${_srv})
+    else()
+      message(FATAL_ERROR "Unknown message type \"${TYPE}\" (must be either STATIC or GENERATED)")
+    endif()
+
+    get_filename_component(_fname ${_srv} NAME)
+    string(REPLACE ".srv" ".h" _output_cpp_base ${_fname})
+
+    set(_outdir ${ROSBUILD_GEN_DIR}/cpp)
+    set(_output_cpp ${_outdir}/${PROJECT_NAME}/${_output_cpp_base})
+    
+    list(APPEND ${PROJECT_NAME}_generated ${_output_cpp})
+
+    set(_incflags "")
+    foreach(dir ${DEPENDED_PACKAGE_PATHS})
+      list(APPEND _incflags -I${dir})
+    endforeach()
+
+    # Add the rule to build the .h from the .srv
+    add_custom_command(
+      OUTPUT ${_output_cpp} 
+      COMMAND ${ROSBUILD_SUBSHELL} 
+      ${gensrv_cpp_exe} 
+      ${_input}
+      -p ${PROJECT_NAME}
+      -o ${_outdir}
+      ${_incflags} -I${CMAKE_CURRENT_SOURCE_DIR}
+      DEPENDS ${_input} ${gensrv_cpp_exe} ${genmsg_cpp_exe} ${gendeps_exe} 
+      COMMENT "${PROJECT_NAME}: generating ${_output_cpp_base}"
+      )
+
+  endforeach(_srv)
+
+endmacro(gensrv_cpp)
+
+macro(gentargets_cpp)
+  add_custom_target(${PROJECT_NAME}_gen_cpp
+    DEPENDS ${${PROJECT_NAME}_generated})
+  install(DIRECTORY ${ROSBUILD_GEN_DIR}/cpp/${PROJECT_NAME} 
+    DESTINATION include
+    OPTIONAL
+    COMPONENT ${PROJECT_NAME})
+
+endmacro()

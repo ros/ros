e60f70682e38a674951cd4701d035cf15fa39e8e
straszheim <troy@industrial-perception.com>
2011-03-10 21:55:29 +0000

    rosbuild2 stuff

---
+++ utilities/xmlrpcpp/rosbuild.cmake
@@ -0,0 +1,36 @@
+if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
+   include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
+endif()
+rosbuild_add_library(XmlRpc 
+  src/XmlRpcClient.cpp 
+  src/XmlRpcDispatch.cpp 
+  src/XmlRpcServer.cpp 
+  src/XmlRpcServerConnection.cpp 
+  src/XmlRpcServerMethod.cpp 
+  src/XmlRpcSocket.cpp 
+  src/XmlRpcSource.cpp 
+  src/XmlRpcUtil.cpp 
+  src/XmlRpcValue.cpp
+  )
+
+foreach(header
+    src/base64.h	      
+    src/XmlRpcException.h	    
+    src/XmlRpcServer.h	      
+    src/XmlRpcSource.h
+    src/XmlRpcClient.h    
+    src/XmlRpc.h		    
+    src/XmlRpcServerMethod.h  
+    src/XmlRpcUtil.h
+    src/XmlRpcDispatch.h  
+    src/XmlRpcServerConnection.h  
+    src/XmlRpcSocket.h	      
+    src/XmlRpcValue.h)
+  get_filename_component(base ${header} NAME)
+
+  install(FILES ${header}
+    DESTINATION include
+    RENAME ${base}
+    )
+endforeach()
+ 

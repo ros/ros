04606e43d0a933209365f0c811a68f92d896755b
straszheim <troy@industrial-perception.com>
2011-03-23 03:01:22 +0000

    rosbuild2 taking shape.

--- tools/rosout/rosbuild.cmake
+++ tools/rosout/rosbuild.cmake
@@ -1,5 +1,3 @@
-if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-  include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-endif()
-rosbuild_add_executable(rosout rosout.cpp)
+include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
 
+rosbuild_add_executable(rosout rosout.cpp)

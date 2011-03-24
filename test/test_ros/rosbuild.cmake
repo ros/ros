b9009a1e657665cb4a745170089a6618d3293d08
straszheim <troy@industrial-perception.com>
2011-03-24 00:26:31 +0000

    rosbuild2 taking shape.

--- test/test_ros/rosbuild.cmake
+++ test/test_ros/rosbuild.cmake
@@ -1,5 +1,3 @@
-if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-  include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-endif()
+include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
 rosbuild_add_rostest(test/test-param-server.xml)
 

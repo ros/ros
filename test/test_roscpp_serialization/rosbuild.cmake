2e72084379a972f6d15bc9fa461f138122342e0a
straszheim <troy@industrial-perception.com>
2011-04-14 21:46:30 +0000

    rosbuild2 manifest stuff

--- test/test_roscpp_serialization/rosbuild.cmake
+++ test/test_roscpp_serialization/rosbuild.cmake
@@ -1,9 +1,8 @@
-if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-  include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-endif()
+include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
 #set the default path for built executables to the "bin" directory
 #set the default path for built libraries to the "lib" directory
 include_directories(include)
+
 rosbuild_add_gtest(serialization src/serialization.cpp)
 rosbuild_add_gtest(generated_messages src/generated_messages.cpp)
 rosbuild_add_executable(builtin_types src/builtin_types.cpp)

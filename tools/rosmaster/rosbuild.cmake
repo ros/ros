dfd6077842d564fc6f9a3c9bb1a78ca3d018b95b
straszheim <troy@industrial-perception.com>
2011-04-19 21:37:44 +0000

    more rosbuild2 preparations

--- tools/rosmaster/rosbuild.cmake
+++ tools/rosmaster/rosbuild.cmake
@@ -1,6 +1,2 @@
-if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-  include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-endif()
-#set the default path for built executables to the "bin" directory
-#set the default path for built libraries to the "lib" directory
+include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
 

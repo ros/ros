16ecc06f8caf41ce7eeb637771b0e2df180900da
straszheim <troy@industrial-perception.com>
2011-05-08 21:18:41 +0000

    cleanup

--- clients/cpp/roscpp/rosbuild.cmake
+++ clients/cpp/roscpp/rosbuild.cmake
@@ -1,7 +1,5 @@
-if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-  include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-endif()
-#set(ROS_BUILD_TYPE Debug)
+include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
+
 add_subdirectory(src)
 add_subdirectory(test EXCLUDE_FROM_ALL)
 

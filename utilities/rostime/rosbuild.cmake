243962b0dfb991493f2117964be79d5bb83492b6
straszheim <troy@industrial-perception.com>
2011-05-08 21:07:48 +0000

    mingw fix, ticket #3483, thanks snorri

--- utilities/rostime/rosbuild.cmake
+++ utilities/rostime/rosbuild.cmake
@@ -1,19 +1,13 @@
-if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-  include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-endif()
-# Set the build type.  Options are:
-#  Coverage       : w/ debug symbols, w/o optimization, w/ code-coverage
-#  Debug          : w/ debug symbols, w/o optimization
-#  Release        : w/o debug symbols, w/ optimization
-#  RelWithDebInfo : w/ debug symbols, w/ optimization
-#  MinSizeRel     : w/o debug symbols, w/ optimization, stripped binaries
-#set(ROS_BUILD_TYPE RelWithDebInfo)
-#set the default path for built executables to the "bin" directory
-#set the default path for built libraries to the "lib" directory
+include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
+
 rosbuild_add_boost_directories()
-rosbuild_add_library(${PROJECT_NAME} src/time.cpp src/rate.cpp src/duration.cpp)
+
+rosbuild_add_library(${PROJECT_NAME} 
+  src/time.cpp src/rate.cpp src/duration.cpp)
+
 rosbuild_link_boost(${PROJECT_NAME} thread)
-if(NOT APPLE OR WIN32)
-target_link_libraries(${PROJECT_NAME} rt)
+
+if(NOT (APPLE OR WIN32 OR MINGW))
+  target_link_libraries(${PROJECT_NAME} rt)
 endif()
 

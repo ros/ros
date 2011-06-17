98014064aba4d26e85d66ac18054ff286be1206d
Ken Conley <kwc@kwc.org>
2011-06-17 17:38:45 +0000

    removing deprecated util

--- core/roslib/src/roslib/rostime.py
+++ core/roslib/src/roslib/rostime.py
@@ -49,20 +49,6 @@ def _canon(secs, nsecs):
         nsecs += 1000000000
     return secs,nsecs
 
-import warnings
-def deprecated(func):
-    """This is a decorator which can be used to mark functions
-    as deprecated. It will result in a warning being emmitted
-    when the function is used."""
-    def newFunc(*args, **kwargs):
-        warnings.warn("Call to deprecated function %s." % func.__name__,
-                      category=DeprecationWarning, stacklevel=2)
-        return func(*args, **kwargs)
-    newFunc.__name__ = func.__name__
-    newFunc.__doc__ = func.__doc__
-    newFunc.__dict__.update(func.__dict__)
-    return newFunc
-
 class TVal(object):
     """
     Base class of L{Time} and L{Duration} representations. Representation

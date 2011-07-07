9e0ac96806faba90c06915b773ef251af99025a3
Tully Foote <tfoote@willowgarage.com>
2011-07-07 00:37:40 +0000

    updating for #3518 freebsd now on python 2.7

--- tools/rosdep/src/rosdep/freebsd.py
+++ tools/rosdep/src/rosdep/freebsd.py
@@ -44,11 +44,11 @@ def port_detect(p):
     if p == "gtk20":
         portname = "gtk-2.\*"
     elif p == "py-gtk2":
-        portname = "py26-gtk-2.\*"
+        portname = "py27-gtk-2.\*"
     elif p[:9] in ["autoconf2", "automake1"]:
         portname = p[:8] + "-" + p[8] + "." + p[9:] + "\*"
     elif p[:3] == "py-":
-        portname = "py26-" + p[3:] + "\*"
+        portname = "py27-" + p[3:] + "\*"
     else:
         portname = p + "-\*"
     pop = subprocess.Popen("/usr/sbin/pkg_info -qE " + portname, shell=True)

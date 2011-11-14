3a9f03fdc517226669627c4ed7a5d559703f70d2
mkjaergaard <mkjaergaard@willowgarage.com>
2011-11-14 22:27:21 +0000

    Support for catkin

---
+++ catkin.cmake
@@ -0,0 +1,14 @@
+cmake_minimum_required(VERSION 2.8)
+project(ros_comm)
+find_package(catkin)
+
+foreach(subdir
+    messages/rosgraph_msgs
+    utilities/xmlrpcpp
+    tools/rosconsole
+    clients/cpp/roscpp
+    )
+  add_subdirectory(${subdir})
+endforeach()
+
+catkin_package(ros_comm)

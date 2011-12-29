3fcac51c9d50019fa6d284e61a0247ae63391d41
straszheim <troy@industrial-perception.com>
2011-12-29 15:10:48 +0000

    make this shared

--- utilities/message_filters/catkin.cmake
+++ utilities/message_filters/catkin.cmake
@@ -5,7 +5,7 @@ include_directories(include)
 include_directories(include/message_filters) # <-weird
 include_directories(${ROS_INCLUDE_DIRS})
 
-add_library(message_filters src/connection.cpp)
+add_library(message_filters SHARED src/connection.cpp)
 
 install_cmake_infrastructure(message_filters
   VERSION 0.0.0

c96cfcb0057aeab903f60ad530f5e6daf5aeb6e9
Brian Gerkey <brian@gerkey.org>
2012-01-02 01:51:51 +0000

    switching to catkin

--- tools/rosbash/catkin.cmake
+++
@@ -1,7 +0,0 @@
-cmake_minimum_required(VERSION 2.8)
-project(rosbash)
-install(FILES rosbash rostcsh roszsh
-        DESTINATION share/ros/rosbash)
-# TODO: manifests should go in automatically
-install(FILES manifest.xml
-        DESTINATION share/ros/rosbash)

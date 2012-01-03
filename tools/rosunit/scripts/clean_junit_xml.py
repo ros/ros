941f5125140e9de3a9ea84cbf6f5cf8d75a38b5c
Brian Gerkey <brian@gerkey.org>
2012-01-03 07:54:32 +0000

    specifically remove 0x1b, which is causing Jenkins a headache

--- tools/rosunit/scripts/clean_junit_xml.py
+++ tools/rosunit/scripts/clean_junit_xml.py
@@ -84,7 +84,8 @@ def clean_results(test_results_dir, output_dir, filter):
                 output_path = os.path.join(output_dir, "%s.xml"%test_name)
                 with open(output_path, 'w') as f:
                     print("re-writing", output_path)
-                    f.write(result.xml().encode('utf-8'))
+                    xml = result.xml().replace('\x1b', '')
+                    f.write(xml.encode('utf-8'))
             except Exception as e:
                 sys.stderr.write("ignoring [%s]: %s\n"%(file, e))
 

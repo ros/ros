02118d9c72a223258f39e3ad714a2c9f1addac98
Ken Conley <kwc@kwc.org>
2012-01-03 23:43:54 +0000

    ros: converting to top-level setup.py that installs 'roslib' (plus rosmake, rosclean, and rosunit)

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import sys
+sys.path.insert(0, 'src')
+
+setup(name='roslib',
+      version= '1.7.1',
+      packages=['ros', 'roslib', 'rosmake', 'rosunit', 'rosclean'],
+      package_dir = {'ros':'core/roslib/src/ros',
+                     'roslib':'core/roslib/src/roslib',
+                     'rosunit':'tools/rosunit/src/rosunit',
+                     'rosmake':'tools/rosmake/src/rosmake',
+                     'rosclean':'tools/rosclean/src/rosclean',
+                     },
+      install_requires=['rospkg'], 
+      scripts = ['tools/rosclean/scripts/rosclean',
+                 'tools/rosmake/scripts/rosmake',
+                 ],
+      author = "Ken Conley", 
+      author_email = "kwc@willowgarage.com",
+      url = "http://www.ros.org/wiki/roslib",
+      download_url = "http://pr.willowgarage.com/downloads/roslib/", 
+      keywords = ["ROS"],
+      classifiers = [
+        "Programming Language :: Python", 
+        "License :: OSI Approved :: BSD License" ],
+      description = "roslib", 
+      long_description = """\
+Internal Python libraries for low-level 'ros' stack.  This also installs the rosmake and rosclean tools.
+This does *not* include the ROS communication libraries.
+""",
+      license = "BSD"
+      )

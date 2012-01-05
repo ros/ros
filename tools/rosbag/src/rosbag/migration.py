f17aef5bcb663aad4e4e26fc51a2026e0ce463b4
kwc <kwc@willowgarage.com>
2012-01-05 04:59:40 +0000

    getting rid of load_manifest calls

--- tools/rosbag/src/rosbag/migration.py
+++ tools/rosbag/src/rosbag/migration.py
@@ -30,9 +30,6 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-PKG = 'rosbag'
-import roslib; roslib.load_manifest(PKG)
-
 import collections
 import copy
 from cStringIO import StringIO

8e9428e2f209c0919f1b27a0ece87671969824aa
kwc <kwc@willowgarage.com>
2012-01-05 04:05:05 +0000

    test_rosgraph merged into rosgraph

--- test/test_rosgraph/rosbuild.cmake
+++
@@ -1,7 +0,0 @@
-if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-  include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-endif()
-rosbuild_add_pyunit(test/test_rosgraph_command_offline.py)
-rosbuild_add_pyunit(test/test_rosgraph_masterapi_offline.py)
-rosbuild_add_rostest(test/masterapi.test)
-

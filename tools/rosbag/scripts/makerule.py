1e34bec821ecc624f0d61db5e14a4105dec707da
kwc <kwc@willowgarage.com>
2012-01-13 21:00:51 +0000

    rosbag makerule.py: convert to genpy and also convert prints to py3k style

--- tools/rosbag/scripts/makerule.py
+++ tools/rosbag/scripts/makerule.py
@@ -31,11 +31,16 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+from __future__ import print_function
+
 import optparse
 import os
 import re
 import sys
 import rosbag.migration
+
+import genpy.message
+import genpy.dynamic
 
 def print_trans(old, new, indent):
     from_txt = '%s [%s]' % (old._type, old._md5sum)
@@ -43,8 +48,8 @@ def print_trans(old, new, indent):
         to_txt= '%s [%s]' % (new._type, new._md5sum)
     else:
         to_txt = 'Unknown'
-    print '    ' * indent + ' * From: %s' % from_txt
-    print '    ' * indent + '   To:   %s' % to_txt
+    print('    ' * indent + ' * From: %s' % from_txt)
+    print('    ' * indent + '   To:   %s' % to_txt)
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage='usage: makerule.py msg.saved [-a] output_rulefile [rulefile1, rulefile2, ...] [-n]')
@@ -58,11 +63,11 @@ if __name__ == '__main__':
     rulefile = args[1]
 
     if os.path.isfile(rulefile) and not options.append:
-        print >> sys.stderr, "The file %s already exists.  Include -a if you intend to append." % rulefile
+        print("The file %s already exists.  Include -a if you intend to append." % rulefile, file=sys.stderr)
         exit(1)
 
     if not os.path.isfile(rulefile) and options.append:
-        print >> sys.stderr, "The file %s does not exist, and so -a is invalid." % rulefile
+        print("The file %s does not exist, and so -a is invalid." % rulefile, file=sys.stderr)
         exit(1)
 
     if options.append:
@@ -72,24 +77,24 @@ if __name__ == '__main__':
 
     f = open(args[0])
     if f is None:
-        print >> sys.stderr, 'Could not open message full definition: %s'
+        print('Could not open message full definition: %s', file=sys.stderr)
         sys.exit()
 
     type_line = f.readline()
     pat = re.compile(r"\[(.*)]:")
     type_match = pat.match(type_line)
     if type_match is None:
-        print >> sys.stderr, "Full definition file malformed.  First line should be: '[my_package/my_msg]:'"
+        print("Full definition file malformed.  First line should be: '[my_package/my_msg]:'", file=sys.stderr)
         sys.exit()
 
     old_type = type_match.groups()[0]
     old_full_text = f.read()
     f.close()
 
-    old_class = roslib.genpy.generate_dynamic(old_type,old_full_text)[old_type]
+    old_class = genpy.dynamic.generate_dynamic(old_type,old_full_text)[old_type]
 
     if old_class is None:
-        print >> sys.stderr, 'Could not generate class from full definition file.'
+        print('Could not generate class from full definition file.', file=sys.stderr)
         sys.exit()
 
     mm = rosbag.migration.MessageMigrator(args[2:]+append_rule,not options.noplugins)
@@ -97,10 +102,10 @@ if __name__ == '__main__':
     migrations = rosbag.migration.checkmessages(mm, [old_class])
 
     if migrations == []:
-        print 'Saved definition is up to date.'
+        print('Saved definition is up to date.')
         exit(0)
 
-    print 'The following migrations need to occur:'
+    print('The following migrations need to occur:')
 
     all_rules = []
     for m in migrations:
@@ -108,18 +113,18 @@ if __name__ == '__main__':
 
         print_trans(m[0][0].old_class, m[0][-1].new_class, 0)
         if len(m[1]) > 0:
-            print "    %d rules missing:" % len(m[1])
+            print("    %d rules missing:" % len(m[1]))
             for r in m[1]:
                 print_trans(r.old_class, r.new_class, 1)
 
     if rulefile is None:
-        print "rulefile not specified"
+        print("rulefile not specified")
     else:
         output = ''
         rules_left = mm.filter_rules_unique(all_rules)
 
         if rules_left == []:
-            print "\nNo additional rule files needed to be generated.  %s not created." % rulefile
+            print("\nNo additional rule files needed to be generated.  %s not created." % rulefile)
             exit(0)
 
         while rules_left != []:
@@ -127,19 +132,19 @@ if __name__ == '__main__':
 
             for r in rules_left:
                 if r.new_class is None:
-                    print "The message type %s appears to have moved.  Please enter the type to migrate it to." % r.old_class._type
+                    print("The message type %s appears to have moved.  Please enter the type to migrate it to." % r.old_class._type)
                     new_type = raw_input('>')
-                    new_class = roslib.message.get_message_class(new_type)
+                    new_class = genpy.message.get_message_class(new_type)
                     while new_class is None:
-                        print "\'%s\' could not be found in your system.  Please make sure it is built." % new_type
+                        print("\'%s\' could not be found in your system.  Please make sure it is built." % new_type)
                         new_type = raw_input('>')
-                        new_class = roslib.message.get_message_class(new_type)
+                        new_class = genpy.message.get_message_class(new_type)
                     new_rule = mm.make_update_rule(r.old_class, new_class)
                     R = new_rule(mm, 'GENERATED.' + new_rule.__name__)
                     R.find_sub_paths()
                     new_rules = [r for r in mm.expand_rules(R.sub_rules) if r.valid == False]
                     extra_rules.extend(new_rules)
-                    print 'Creating the migration rule for %s requires additional missing rules:' % new_type
+                    print('Creating the migration rule for %s requires additional missing rules:' % new_type)
                     for nr in new_rules:
                         print_trans(nr.old_class, nr.new_class,1)
                     output += R.get_class_def()
@@ -149,4 +154,4 @@ if __name__ == '__main__':
         f = open(rulefile, 'a')
         f.write(output)
         f.close()
-        print "\nThe necessary rule files have been written to: %s" % rulefile
+        print("\nThe necessary rule files have been written to: %s" % rulefile)

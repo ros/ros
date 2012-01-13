e7f68650012b24736f13891f6bd2241d8f4a7a12
gerkey <gerkey@willowgarage.com>
2012-01-13 06:03:32 +0000

    catkinized

--- test/test_roscpp_serialization_perf/rosbuild.cmake
+++
@@ -1,5 +0,0 @@
-include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-rosbuild_add_executable(pointcloud_serdes pointcloud_serdes.cpp)
-#rosbuild_add_compile_flags(pointcloud_serdes "-O3 -funroll-loops")
-#rosbuild_add_compile_flags(pointcloud_serdes "-march=prescott")
-

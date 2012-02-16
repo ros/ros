01b3fc84950a5266688be8dc69ce8543bfb3fd51
kwc <kwc@willowgarage.com>
2012-02-16 20:48:03 +0000

    #3799: fix double-encoding of utf-8.  Unfortunately this is not py3k safe

--- tools/roslaunch/src/roslaunch/xmlloader.py
+++ tools/roslaunch/src/roslaunch/xmlloader.py
@@ -711,7 +711,7 @@ class XmlLoader(loader.Loader):
         try:
             if verbose:
                 print("... loading XML")
-            if hasattr(xml_text,'encode'):
+            if hasattr(xml_text,'encode') and isinstance(xml_text, unicode):
                 # #3799: xml_text comes in a unicode object, which
                 # #fails since XML text is expected to be encoded.
                 # that's why force encoding to utf-8 here (make sure XML header is utf-8)

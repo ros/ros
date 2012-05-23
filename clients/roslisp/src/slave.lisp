f9af6ef3d0dcf72f13d75478e80c23393ab03b73
lorenz <lorenz>
2012-05-23 11:54:52 +0000

    Fixed indentation of slave.lisp.

--- clients/roslisp/src/slave.lisp
+++ clients/roslisp/src/slave.lisp
@@ -123,17 +123,17 @@ Notes
   (ros-debug (roslisp slave request-topic) "Received call `requestTopic ~a ~a ~a" caller-id topic protocols)
   (with-recursive-lock (*ros-lock*)
     (if (find "TCPROS" protocols :key #'first :test #'string=)
-	(if (hash-table-has-key *publications* topic)
-	    
-	    ;; TCPROS-specific
-	    (list 1 (format nil "ready on ~a:~a" *tcp-server-hostname* *tcp-server-port*)
-		  (list "TCPROS" *tcp-server-hostname* *tcp-server-port*))
+        (if (hash-table-has-key *publications* topic)
+            
+            ;; TCPROS-specific
+            (list 1 (format nil "ready on ~a:~a" *tcp-server-hostname* *tcp-server-port*)
+                  (list "TCPROS" *tcp-server-hostname* *tcp-server-port*))
 
-	    ;; If I don't know about this topic
-	    (list -1 (format nil "Not a publisher of ~a" topic) nil))
+            ;; If I don't know about this topic
+            (list -1 (format nil "Not a publisher of ~a" topic) nil))
 
-	;; If TCPROS is not on the protocol list
-	(list 0 (format nil "Protocol list ~a does not contain TCPROS" protocols) nil))))
+        ;; If TCPROS is not on the protocol list
+        (list 0 (format nil "Protocol list ~a does not contain TCPROS" protocols) nil))))
 
 
 
@@ -152,10 +152,10 @@ Notes
   ;; In that case, replace it with the empty list
   (when (stringp publishers)
     (if (= 0 (length (string-trim '(#\Space #\Tab #\Newline) publishers)))
-	(setf publishers nil)
-	(progn
-	  (ros-error roslisp "In update publishers, got a string ~a rather than a list of publishers.  Skipping." publishers)
-	  (return-from update-publishers))))
+        (setf publishers nil)
+        (progn
+          (ros-error roslisp "In update publishers, got a string ~a rather than a list of publishers.  Skipping." publishers)
+          (return-from update-publishers))))
 
   ;; Get the current info for this topic
   (mvbind (subscription known) (gethash topic *subscriptions*)
@@ -164,20 +164,20 @@ Notes
 
        ;; Remove no-longer-existing publisher connections
        (setf (publisher-connections subscription)
-	     (delete-if #'(lambda (conn) (not (member (uri conn) publishers :test #'equal))) 
-			(publisher-connections subscription)))
+             (delete-if #'(lambda (conn) (not (member (uri conn) publishers :test #'equal))) 
+                        (publisher-connections subscription)))
 
        ;; Add and subscribe to new ones
        (dolist (pub publishers (list 1 "updated" 0))
-	 (unless (member pub (publisher-connections subscription) :test #'equal :key #'uri)
+         (unless (member pub (publisher-connections subscription) :test #'equal :key #'uri)
 
-	   ;; TCPROS-specific to assume connection consists of a socket and a stream
-	   (handler-case
-	       (mvbind (conn str) (subscribe-publisher pub topic)
-		 (push (make-publisher-connection :publisher-socket conn :publisher-stream str :uri pub)
-		       (publisher-connections subscription)))
-	     (sb-bsd-sockets:connection-refused-error (c) (ros-debug (roslisp tcp) "Socket error ~a when attempting to subscribe to ~a; skipping" c pub)))
-	     )))
+           ;; TCPROS-specific to assume connection consists of a socket and a stream
+           (handler-case
+               (mvbind (conn str) (subscribe-publisher pub topic)
+                 (push (make-publisher-connection :publisher-socket conn :publisher-stream str :uri pub)
+                       (publisher-connections subscription)))
+             (sb-bsd-sockets:connection-refused-error (c) (ros-debug (roslisp tcp) "Socket error ~a when attempting to subscribe to ~a; skipping" c pub)))
+           )))
 
       ((not known) (list 0 (format nil "I'm not interested in topic ~a" topic) 0)))))
 
@@ -194,12 +194,9 @@ Right now, the transport must be TCPROS and the return value is the socket."
     status. Change *xmlrpc-timeout* to increase wait-time." :format-arguments (list uri topic) ))
     (handler-case
         (return
-  (dbind (protocol address port) (with-function-timeout *xmlrpc-timeout* (lambda () (ros-rpc-call uri "requestTopic" topic (list (list "TCPROS")))))
-    (if (string= protocol "TCPROS")
-	(setup-tcpros-subscription address port topic)
-	(ros-error (roslisp tcp) "Protocol ~a did not equal TCPROS... skipping connection" protocol))))
-  (function-timeout () ;;just retry
+          (dbind (protocol address port) (with-function-timeout *xmlrpc-timeout* (lambda () (ros-rpc-call uri "requestTopic" topic (list (list "TCPROS")))))
+            (if (string= protocol "TCPROS")
+                (setup-tcpros-subscription address port topic)
+                (ros-error (roslisp tcp) "Protocol ~a did not equal TCPROS... skipping connection" protocol))))
+      (function-timeout () ;;just retry
         nil))))
-
-
-

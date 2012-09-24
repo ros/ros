aeb6753c9393560c5f013b78e55666d61a7cefee
Dave Hershberger <hersh@willowgarage.com>
2012-09-24 14:43:03 -0700

    rosservice now has package.xml, setup.py, and CMakeLists.txt.  rosgraph adds catkin build dependency.

---
+++ tools/rosservice/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.package import parse_package_for_distutils
+
+d = parse_package_for_distutils()
+d['packages'] = ['rosservice']
+d['package_dir'] = {'': 'src'}
+d['scripts'] = ['scripts/rosservice']
+d['install_requires'] = ['rospkg', 'genmsg', 'genpy', 'roslib']
+
+setup(**d)

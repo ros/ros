6d234b05f070b0f292b48a54ef25b99fa8e8d923
Dave Hershberger <hersh@willowgarage.com>
2012-09-24 10:15:37 -0700

    ported rostopic to new catkin.

---
+++ tools/rostopic/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.package import parse_package_for_distutils
+
+d = parse_package_for_distutils()
+d['packages'] = ['rostopic']
+d['package_dir'] = {'': 'src'}
+d['scripts'] = ['scripts/rostopic']
+d['install_requires'] = ['rospkg', 'genmsg', 'genpy', 'roslib']
+
+setup(**d)

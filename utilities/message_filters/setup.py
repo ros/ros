f25397b282ed83c4b15e0cc84410f9f4ab5fcc28
Dave Hershberger <hersh@willowgarage.com>
2012-09-24 10:49:01 -0700

    rosmsg, rosnode, message_filters, and roswtf ported to latest catkin.  rosgraph/package.xml got a new test_depend on mock.

---
+++ utilities/message_filters/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.package import parse_package_for_distutils
+
+d = parse_package_for_distutils()
+d['packages'] = ['message_filters']
+d['package_dir'] = {'': 'src'}
+d['install_requires'] = ['rospkg', 'genmsg', 'genpy', 'roslib']
+
+setup(**d)
---
+++ utilities/roswtf/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.package import parse_package_for_distutils
+
+d = parse_package_for_distutils()
+d['packages'] = ['roswtf']
+d['package_dir'] = {'': 'src'}
+d['scripts'] = ['scripts/roswtf']
+d['install_requires'] = ['rospkg', 'genmsg', 'genpy', 'roslib']
+
+setup(**d)

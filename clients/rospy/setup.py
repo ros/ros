b9be30580c38e2ee87e4f0996c0fbf111d8af282
Dave Hershberger <hersh@hairynipples.(none)>
2012-09-24 08:01:44 -0700

    rospy package updated: package.xml, setup.py, CMakeLists.txt.  rostest also updated to depend on rospy for test running.

--- clients/rospy/setup.py
+++ clients/rospy/setup.py
@@ -4,9 +4,8 @@ from distutils.core import setup
 from catkin_pkg.package import parse_package_for_distutils
 
 d = parse_package_for_distutils()
-d['packages'] = ['rostest']
+d['packages'] = ['rospy', 'rospy.impl']
 d['package_dir'] = {'': 'src'}
-d['scripts'] = ['scripts/rostest']
-d['install_requires'] = ['rospkg', 'genmsg', 'genpy', 'roslib', 'rospy']
+d['install_requires'] = ['rospkg', 'genmsg', 'genpy', 'roslib']
 
 setup(**d)

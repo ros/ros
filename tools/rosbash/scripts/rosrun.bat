ec35aa48b4bb7a97bd6e68f6db3e9198dd64d568
Sean Yen <seanyen@microsoft.com>
2018-08-23 12:47:09 -0700

    rosrun_fix

--- tools/rosbash/scripts/rosrun.bat
+++ tools/rosbash/scripts/rosrun.bat
@@ -1,5 +1,6 @@
 @echo off
 REM setlocal EnableDelayedExpansion
+setlocal
 
 set args=0
 set rosrun_prefix=""
@@ -35,6 +36,8 @@ if %args% gtr 0 goto :handleargs
 :rosrun
 if %args% lss 2 goto :usage
 
+set catkin_package_libexec_dirs=
+set pkgdir=
 if NOT EXIST %CMAKE_PREFIX_PATH% (
   set prevline=
   for /f "delims=" %%g in ('catkin_find --without-underlays --libexec --share %1') do (
@@ -65,31 +68,31 @@ if NOT "%wildchar%" == "*" (
   if NOT "%catkin_package_libexec_dirs%" == "" (
     for %%g in (%catkin_package_libexec_dirs%) do (
       call :findexe %2.exe %%g
-	)
+	  )
   )
   call :findexe %2.exe %pkgdir%
   for /f "delims=" %%a in ('catkin_find --bin') do set "catbin=%%a"
   call :findexe %2.exe %catbin%
   
   REM Select the first exe in the list
-  if %nexes EQU 0 (
+  if %nexes% EQU 0 (
     echo [rosrun] Couldn't find executable named %2.exe
-	exit /b 3
+	  exit /b 3
   )
-  if %nexes GTR 1 (
+  if %nexes% GTR 1 (
     echo [rosrun] You have chosen a non-unique executable, selecting the first.
   )
   
   set nexes=0
   set exepath=%exepaths_0%
 ) else (
-  set absname=%pkgdir%/%2%.exe
-  call :debug "Path given. Looking for %absname%"
-  if NOT EXIST %absname% (
+   set "absname=%pkgdir%\%2.exe"
+   call :debug "Path given. Looking for %absname%"
+   if NOT EXIST "%absname%" (
     echo Couldn't find executable named %absname%
     exit /b 3
-  )
-  set exepath=%absname%
+   )
+   set exepath=%absname%
 )
 
 :rosrun

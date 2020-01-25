2ad293622d1a1fa615a751ce1bafa455c824ab21
Shane Loretz <sloretz@openrobotics.org>
2020-01-24 16:46:16 -0800

    Use setuptools instead of distutils (#235)
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- tools/rosboost_cfg/setup.py
+++ tools/rosboost_cfg/setup.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
+from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
